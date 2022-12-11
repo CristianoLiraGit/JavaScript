0e08476c7e185ffce72a251ac6a85e1371e1779a
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2022-12-10 21:32:14 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60
+console.log(`A velocidade do carro é ${vel} km por hora`)
+if (vel > 60) { // condição simples. Não tem else
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+console.log(`Dirija sempre usando cinto de segurança`)
